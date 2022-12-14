a0a48900666bcd5849dfffb24aac199e32d18ea9
pan-asovsky <panosovski@gmail.com>
2022-12-14 22:28:14 +0000

    init

--- grpc/client/client.go
+++ grpc/client/client.go
@@ -1,1 +1,27 @@
-package client
+package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	pb "goprojects/grpc/gen/proto"
+	"log"
+)
+
+func main() {
+
+	conn, err := grpc.Dial("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Println(err)
+	}
+
+	client := pb.NewTestAPIClient(conn)
+	resp, err := client.Echo(context.Background(), &pb.ResponseRequest{Msg: "Hello, world!"})
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println(resp)
+	fmt.Println(resp.Msg)
+}
--- grpc/server/server.go
+++ grpc/server/server.go
@@ -1,1 +1,38 @@
-package server
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	pb "goprojects/grpc/gen/proto"
+	"log"
+	"net"
+)
+
+type testApiServer struct {
+	pb.UnimplementedTestAPIServer
+}
+
+func (s *testApiServer) Echo(ctx context.Context, req *pb.ResponseRequest) (*pb.ResponseRequest, error) {
+	return req, nil
+}
+
+func (s *testApiServer) GetUser(ctx context.Context, req *pb.UserRequest) (*pb.UserResponse, error) {
+	return &pb.UserResponse{}, nil
+}
+
+func main() {
+
+	listener, err := net.Listen("tcp", "localhost:8080")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterTestAPIServer(grpcServer, &testApiServer{})
+
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Println(err)
+	}
+}
